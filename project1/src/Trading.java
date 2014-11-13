0d1e83816e286b69e0ffd657882d47475dc39016
Nick Otero <notero87@gmail.com>
2014-11-13 03:14:05 -0500

    Made the code more robust

--- project1/src/Trading.java
+++ project1/src/Trading.java
@@ -23,13 +23,22 @@ public class Trading {
 		sales = new ArrayList<Transaction>(50);
 
 		for (String bid : orders) {
-			bid = bid.replace(" ", "").replace("(", "").replace("(", "").replace(")", "");
-			String[] typePriceQuant = bid.split(",");
-			
-			Bid newBid = new Bid(typePriceQuant[0],
-					Integer.parseInt(typePriceQuant[1]),
-					Integer.parseInt(typePriceQuant[2]));
-			
+			bid = bid.replace("(", "").replace("(", "")
+					.replace(")", "").replace(",", "")
+					.trim().replaceAll(" +", " ");
+			String[] typePriceQuant = bid.split(" ");
+
+			Bid newBid;
+			try {
+				newBid = new Bid(typePriceQuant[0],
+						Integer.parseInt(typePriceQuant[1]),
+						Integer.parseInt(typePriceQuant[2]));
+			} catch (Exception e) {
+				System.out.println("Bad input: Exiting due to exception: "
+						+ e.toString());
+				return;
+			}
+
 			if (newBid.getType().equals("buy")) {
 				buyOrders.add(newBid);
 			} else if (newBid.getType().equals("sell")) {
@@ -39,70 +48,67 @@ public class Trading {
 						+ newBid.getType());
 				return;
 			}
-			System.out.println(buyOrders.toString());
 			processMarket();
 		}
-		
+
 		if (orders.length > 0)
-		System.out.println("on input: " + Arrays.toString(orders));
+			System.out.println("on input: " + Arrays.toString(orders));
 		System.out.print("the sequence of sales is: [");
-		for (int i = 0; i < sales.size(); i++){
+		for (int i = 0; i < sales.size(); i++) {
 			System.out.print(sales.get(i).toString());
-			if (i != (sales.size() - 1)){
+			if (i != (sales.size() - 1)) {
 				System.out.print(", ");
 			}
 		}
 		System.out.println("]");
-		
+
 		System.out.print("the outstanding sell orders are: [");
 		int numSell = sellOrders.size();
-		for (int i = 0; i < numSell; i++){
+		for (int i = 0; i < numSell; i++) {
 			System.out.print(sellOrders.poll().toString());
-			if (i != (numSell - 1)){
+			if (i != (numSell - 1)) {
 				System.out.print(", ");
 			}
 		}
 		System.out.println("]");
-		
+
 		System.out.print("the outstanding buy orders are: [");
 		int numBuy = buyOrders.size();
-		for (int i = 0; i < numBuy; i++){
+		for (int i = 0; i < numBuy; i++) {
 			System.out.print(buyOrders.poll().toString());
-			if (i != (numBuy - 1)){
+			if (i != (numBuy - 1)) {
 				System.out.print(", ");
 			}
 		}
 		System.out.println("]");
 	}
 
-	void processMarket(){
-		while (true){
-			if(buyOrders.isEmpty() || sellOrders.isEmpty()){
+	void processMarket() {
+		while (true) {
+			if (buyOrders.isEmpty() || sellOrders.isEmpty()) {
 				return;
 			}
-			
+
 			int biddingPrice = buyOrders.peek().getPrice();
 			int biddingQuant = buyOrders.peek().getQuant();
 			int askingPrice = sellOrders.peek().getPrice();
 			int askingQuant = sellOrders.peek().getQuant();
-			
-			if (biddingPrice >= askingPrice){
+
+			if (biddingPrice >= askingPrice) {
 				Bid buyOrder = buyOrders.poll();
 				Bid sellOrder = sellOrders.poll();
-				
+
 				int quant = biddingQuant;
-				if (biddingQuant < askingQuant){
+				if (biddingQuant < askingQuant) {
 					sellOrder.setQuant(askingQuant - quant);
 					sellOrders.add(sellOrder);
-				}
-				else if (biddingQuant > askingQuant){
+				} else if (biddingQuant > askingQuant) {
 					quant = askingQuant;
 					buyOrder.setQuant(biddingQuant - quant);
 					buyOrders.add(buyOrder);
 				}
 				sales.add(new Transaction(askingPrice, quant));
-			}
-			else {
+			} else {
 				return;
 			}
 		}
