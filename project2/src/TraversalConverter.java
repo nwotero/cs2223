f0f2af1a8f43761335e827db8227f97064f16812
Nick Otero <notero87@gmail.com>
2014-11-22 15:54:23 -0500

    pre_post_to_in has been implemented, but not tested. Not entirely confident on the implementation, it should be reviewed critically.

--- project2/src/TraversalConverter.java
+++ project2/src/TraversalConverter.java
@@ -1,115 +1,191 @@
-
 public class TraversalConverter {
 
-	public static String[] search_pre_to_post(String[] preorder){
-		//Check to make sure the given array is actually a preorder of a binary
-		//search tree
-		
-		//Convert the preorder to a postorder
+	public static String[] search_pre_to_post(String[] preorder) {
+		// Check to make sure the given array is actually a preorder of a binary
+		// search tree
+
+		// Convert the preorder to a postorder
 		return null;
 	}
-	
-	public static String[] pre_in_to_post(String[] preorder, String[] inorder){
-		//Check to make sure the given arrays are actually a preorder and inorder of a binary
-		//search tree
-		if (preorder.length != inorder.length){
+
+	public static String[] pre_in_to_post(String[] preorder, String[] inorder) {
+		// Check to make sure the given arrays are actually a preorder and
+		// inorder of a binary
+		// search tree
+		if (preorder.length != inorder.length) {
 			printError("pre_in_to_post");
 			return new String[0];
-		}
-		else if(preorder.length == 0){
+		} else if (preorder.length == 0) {
 			printError("pre_in_to_post");
 		}
-		
-		//Base case for recursive algorithm
-		if (preorder.length == 1){
-			if (preorder[0] == inorder[0])
-			{
-				String[] baseArray = {preorder[0]};
+
+		// Base case for recursive algorithm
+		if (preorder.length == 1) {
+			if (preorder[0] == inorder[0]) {
+				String[] baseArray = { preorder[0] };
 				return baseArray;
-			}
-			else{
+			} else {
 				printError("pre_in_to_post");
 				return new String[0];
 			}
 		}
-		
-		//Partition the inorder array about the first entry in preorder
+
+		// Partition the inorder array about the first entry in preorder
 		String root = preorder[0];
-		String[] leftSideIn = new String[preorder.length - 1];
-		int leftIndex = 0;
-		String[] rightSideIn = new String[preorder.length - 1];
-		int rightIndex = 0;
-		boolean onLeft = true;
-		
-		for (int i = 0; i < preorder.length; i++){
-			if (inorder[i].equals(root))
-			{
-				onLeft = false;
-			}
-			else if (onLeft){
-				leftSideIn[leftIndex] = inorder[i];
-				leftIndex++;
-			}
-			else{
-				rightSideIn[rightIndex] = inorder[i];
-				rightIndex++;
-			}
-		}
-		
-		if (onLeft){
+		String[][] partitionedInOrder;
+		try {
+			partitionedInOrder = partitionArray(root, inorder);
+		} catch (Exception e) {
 			printError("pre_in_to_post");
 			return new String[0];
 		}
-		
-		//Partition the preorder array using the partitioned inorder array
+		String[] leftSideIn = partitionedInOrder[0];
+		String[] rightSideIn = partitionedInOrder[1];
+
+		// Partition the preorder array using the partitioned inorder array
 		String[] leftSidePre = new String[leftSideIn.length];
 		String[] rightSidePre = new String[rightSideIn.length];
-		
-		for (int j = 0; j < leftSidePre.length; j++){
-			leftSidePre[j] = preorder[j+1]; 
+
+		for (int j = 0; j < leftSidePre.length; j++) {
+			leftSidePre[j] = preorder[j + 1];
 		}
-		
-		for (int k = 0; k < rightSidePre.length; k++){
+
+		for (int k = 0; k < rightSidePre.length; k++) {
 			rightSidePre[k] = preorder[leftSidePre.length + k + 1];
 		}
-		
-		//Use recursion to convert the partitioned arrays into postorder arrays
+
+		// Use recursion to convert the partitioned arrays into postorder arrays
 		String[] leftSidePost = pre_in_to_post(leftSidePre, leftSideIn);
 		String[] rightSidePost = pre_in_to_post(rightSidePre, rightSideIn);
-		String[] rootArray = {root};
-		
-		//Concatenate the postorder arrays and the root.
+		String[] rootArray = { root };
+
+		// Concatenate the postorder arrays and the root.
 		String[] tempArray = concatenateArray(leftSidePost, rightSidePost);
 		String[] returnArray = concatenateArray(tempArray, rootArray);
-		
+
 		return returnArray;
 	}
-	
-	public static String[] pre_post_to_in(String[] preorder, String[] postorder){
-		//Check to make sure the given arrays are actually a preorder and postorder of a binary
-		//search tree
+
+	public static String[] pre_post_to_in(String[] preorder, String[] postorder) {
+		// Check to make sure the given arrays are actually a preorder and
+		// postorder of a binary
+		// search tree
+
+		if (preorder.length != postorder.length) {
+			printError("pre_post_to_in");
+			return new String[0];
+		} else if (preorder.length == 0) {
+			printError("pre_in_to_post");
+		}
+
+		// Base case for recursive algorithm
+		if (preorder.length == 1) {
+			if (preorder[0] == postorder[0]) {
+				String[] baseArray = {preorder[0]};
+				return baseArray;
+			} else {
+				printError("pre_post_to_in");
+				return new String[0];
+			}
+		}
+
+		// Partition the inorder array about the first entry in preorder
+		String root = preorder[0];
+		String[][] partitionedPostOrder;
+		try {
+			partitionedPostOrder = partitionArray(root, postorder);
+		} catch (Exception e) {
+			printError("pre_post_to_in");
+			return new String[0];
+		}
+		String[] leftSidePost = partitionedPostOrder[0];
+		String[] rightSidePost = partitionedPostOrder[1];
+
+		// Partition the preorder array using the partitioned inorder array
+		String[] leftSidePre = new String[leftSidePost.length];
+		String[] rightSidePre = new String[rightSidePost.length];
+
+		for (int j = 0; j < leftSidePre.length; j++) {
+			leftSidePre[j] = preorder[j + 1];
+		}
+
+		for (int k = 0; k < rightSidePre.length; k++) {
+			rightSidePre[k] = preorder[leftSidePre.length + k + 1];
+		}
+
+		// Use recursion to convert the partitioned arrays into postorder arrays
+		String[] leftSideIn = pre_post_to_in(leftSidePre, leftSidePost);
+		String[] rightSideIn = pre_post_to_in(rightSidePre, rightSidePost);
+		String[] rootArray = { root };
+		String[] returnArray = new String[leftSideIn.length + rightSideIn.length + 1];
 		
-		//Convert the preorder and postorder to an inorder
-		return null;
+		if (leftSideIn.length == 0){	//If this node has no children
+			String[] nullArray = {null};
+			String[] tempArray = concatenateArray(rootArray, nullArray);
+			returnArray = concatenateArray(tempArray, rightSideIn);
+		}else if (rightSideIn.length == 0){ //If this node has no neighbors
+			returnArray = concatenateArray(leftSideIn, rootArray);
+		}else{ //If this node has children and neighbors
+			//Iterate through children of root, insert root at null point
+			for (int i = 0; i < leftSideIn.length; i++){
+				if (leftSideIn[i] == null){
+					leftSideIn[i] = root;
+					returnArray = concatenateArray(leftSideIn, rightSideIn); 
+					break;
+				}
+			}
+		}
+		return returnArray;
 	}
-	
-	private static String[] concatenateArray(String[] array1, String[] array2){
+
+	private static String[][] partitionArray(String pivot, String[] array)
+			throws Exception {
+		// Partition the inorder array about the first entry in preorder
+		String[] leftSide = new String[array.length - 1];
+		int leftIndex = 0;
+		String[] rightSide = new String[array.length - 1];
+		int rightIndex = 0;
+		boolean onLeft = true;
+
+		for (int i = 0; i < array.length; i++) {
+			if (array[i].equals(pivot)) {
+				onLeft = false;
+			} else if (onLeft) {
+				leftSide[leftIndex] = array[i];
+				leftIndex++;
+			} else {
+				rightSide[rightIndex] = array[i];
+				rightIndex++;
+			}
+		}
+		if (onLeft) {
+			throw new Exception();
+		}
+
+		String[][] returnArray = new String[2][array.length - 1];
+		returnArray[0] = leftSide;
+		returnArray[1] = rightSide;
+		return returnArray;
+	}
+
+	private static String[] concatenateArray(String[] array1, String[] array2) {
 		int newLength = array1.length + array2.length;
 		String[] returnArray = new String[newLength];
-		
-		for (int i = 0; i < array1.length; i++){
+
+		for (int i = 0; i < array1.length; i++) {
 			returnArray[i] = array1[i];
 		}
-		
-		for (int j = 0; j < array2.length; j++){
+
+		for (int j = 0; j < array2.length; j++) {
 			returnArray[j + array1.length] = array2[j];
 		}
-		
+
 		return returnArray;
 	}
-	
-	private static void printError(String funcName){
-		System.out.println("Error in method " + funcName + ": given arrays cannot"
+
+	private static void printError(String funcName) {
+		System.out.println("Error in method " + funcName
+				+ ": given arrays cannot"
 				+ "be the preorder and inorder traversals of the same tree");
 	}
 }
