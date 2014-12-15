14f6727d81af4cf68f7c77a25d7e74c5dfbd390a
Nick Otero <notero87@gmail.com>
2014-12-14 21:31:38 -0500

    Implemented EWDAM and DijkstraAM. Untested. Needs negative cycle detection.

---
+++ project4/src/project4/EWDAM.java
@@ -0,0 +1,163 @@
+package project4;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class EWDAM {
+
+	private final int V;
+    private int E;
+    private double[][] adj;
+    
+    /**
+     * Initializes an empty edge-weighted digraph with <tt>V</tt> vertices and 0 edges.
+     * param V the number of vertices
+     * @throws java.lang.IllegalArgumentException if <tt>V</tt> < 0
+     */
+    public EWDAM(int V) {
+        if (V < 0) throw new IllegalArgumentException("Number of vertices in a Digraph must be nonnegative");
+        this.V = V;
+        this.E = 0;
+        adj = new double[V][V];
+        for (int i = 0; i < V; i++){
+        	for (int j = 0; i < V; i++){
+        		adj[i][j] = Double.NaN;
+        	}
+        }
+    }
+
+    /**  
+     * Initializes an edge-weighted digraph from an input stream.
+     * The format is the number of vertices <em>V</em>,
+     * followed by the number of edges <em>E</em>,
+     * followed by <em>E</em> pairs of vertices and edge weights,
+     * with each entry separated by whitespace.
+     * @param in the input stream
+     * @throws java.lang.IndexOutOfBoundsException if the endpoints of any edge are not in prescribed range
+     * @throws java.lang.IllegalArgumentException if the number of vertices or edges is negative
+     */
+    public EWDAM(In in) {
+        this(in.readInt());
+        int E = in.readInt();
+        if (E < 0) throw new IllegalArgumentException("Number of edges must be nonnegative");
+        for (int i = 0; i < E; i++) {
+            int v = in.readInt();
+            int w = in.readInt();
+            if (v < 0 || v >= V) throw new IndexOutOfBoundsException("vertex " + v + " is not between 0 and " + (V-1));
+            if (w < 0 || w >= V) throw new IndexOutOfBoundsException("vertex " + w + " is not between 0 and " + (V-1));
+            double weight = in.readDouble();
+            addEdge(new DirectedEdge(v, w, weight));
+        }
+    }
+
+    /**
+     * Returns the number of vertices in the edge-weighted digraph.
+     * @return the number of vertices in the edge-weighted digraph
+     */
+    public int V() {
+        return V;
+    }
+
+    /**
+     * Returns the number of edges in the edge-weighted digraph.
+     * @return the number of edges in the edge-weighted digraph
+     */
+    public int E() {
+        return E;
+    }
+
+    // throw an IndexOutOfBoundsException unless 0 <= v < V
+    private void validateVertex(int v) {
+        if (v < 0 || v >= V)
+            throw new IndexOutOfBoundsException("vertex " + v + " is not between 0 and " + (V-1));
+    }
+
+    /**
+     * Adds the directed edge <tt>e</tt> to the edge-weighted digraph.
+     * @param e the edge
+     * @throws java.lang.IndexOutOfBoundsException unless endpoints of edge are between 0 and V-1
+     */
+    public void addEdge(DirectedEdge e) {
+        int v = e.from();
+        int w = e.to();
+        validateVertex(v);
+        validateVertex(w);
+        adj[v][w] = e.weight();
+        E++;
+    }
+
+
+    /**
+     * Returns the directed edges incident from vertex <tt>v</tt>.
+     * @return the directed edges incident from vertex <tt>v</tt> as an Iterable
+     * @param v the vertex
+     * @throws java.lang.IndexOutOfBoundsException unless 0 <= v < V
+     */
+    public Iterable<DirectedEdge> adj(int v) {
+        validateVertex(v);
+        List<DirectedEdge> iterable = new ArrayList<DirectedEdge>();
+        for (int i = 0; i < V; i++){
+        	for (int j = 0; j < V; j++){
+        		if (adj[i][j] != Double.NaN){
+        			iterable.add(new DirectedEdge(i, j, adj[i][j]));
+        		}
+        	}
+        }
+        return iterable;
+    }
+
+    /**
+     * Returns the number of directed edges incident from vertex <tt>v</tt>.
+     * This is known as the <em>outdegree</em> of vertex <tt>v</tt>.
+     * @return the outdegree of vertex <tt>v</tt>
+     * @param v the vertex
+     * @throws java.lang.IndexOutOfBoundsException unless 0 <= v < V
+     */
+    public int outdegree(int v) {
+        validateVertex(v);
+        int degree = 0;
+        for (int i = 0; i < V; i++){
+        	if (adj[v][i] != Double.NaN){
+        		degree++;
+        	}
+        }
+        return degree;
+    }
+
+    /**
+     * Returns all directed edges in the edge-weighted digraph.
+     * To iterate over the edges in the edge-weighted graph, use foreach notation:
+     * <tt>for (DirectedEdge e : G.edges())</tt>.
+     * @return all edges in the edge-weighted graph as an Iterable.
+     */
+    public Iterable<DirectedEdge> edges() {
+        Bag<DirectedEdge> list = new Bag<DirectedEdge>();
+        for (int v = 0; v < V; v++) {
+        	for (int w = 0; w < V; w++){
+        		list.add(new DirectedEdge(v, w, adj[v][w]));
+        	}
+        }
+        return list;
+    } 
+
+    /**
+     * Returns a string representation of the edge-weighted digraph.
+     * This method takes time proportional to <em>E</em> + <em>V</em>.
+     * @return the number of vertices <em>V</em>, followed by the number of edges <em>E</em>,
+     *   followed by the <em>V</em> adjacency lists of edges
+     *
+    public String toString() {
+        String NEWLINE = System.getProperty("line.separator");
+        StringBuilder s = new StringBuilder();
+        s.append(V + " " + E + NEWLINE);
+        for (int v = 0; v < V; v++) {
+            s.append(v + ": ");
+            for (DirectedEdge e : adj[v]) {
+                s.append(e + "  ");
+            }
+            s.append(NEWLINE);
+        }
+        return s.toString();
+    }*/
+}
